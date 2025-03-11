e87ed65196e6c96ed03a1b84cd70451a791f0cf4
Dan-Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2025-03-11 13:14:04 +0000

    Added resources, converted HTML to JSX and separated the components

--- 07-react-routers-workshop/client/src/App.jsx
+++ 07-react-routers-workshop/client/src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
+import PageCreate from './components/page-create/PageCreate';
+import PageEdit from './components/page-edit/PageEdit';
+import Details from './components/details/Details';
+import Catalog from './components/catalog/Catalog';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
-  return (
-    <>
+	return (
+		<div id="box">
 
-    </>
-  )
+			<Header />
+
+			<main id="main-content">
+				<Home />
+				<Login />
+				<Register />
+				<PageCreate />
+				<PageEdit />
+				<Details />
+				<Catalog />
+			</main>
+
+
+		</div>
+	)
 }
 
-export default App
+export default App;
